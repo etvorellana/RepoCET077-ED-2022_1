233322a5a6c1e7f0029d6833562a408bb45aca36
Alberto Henrique <albertolunia@gmail.com>
2022-03-28 21:08:36 -0300

    mudança
    Mudança na implementação do codigo de hanoi

--- equipe_1/PrimeiraLista/questao4.c
+++ equipe_1/PrimeiraLista/questao4.c
@@ -13,10 +13,13 @@ int main(void){
 }
 
 void hanoi(int qtd, char origem, char destino, char aux){
-  if (qtd > 0)
+  if(qtd == 1){
+    printf("\nMova o disco 1 da base %c --> %c\n", origem, destino);
+    return;
+  }
   {
-    hanoi(qtd-1, origem, aux, destino);
-    printf("%c --> %c\n", origem, destino);
-    hanoi(qtd-1, aux, destino, origem);
+    hanoi(qtd-1, origem, aux, destino); // Resolve para n-1 discos, movendo para o disco aux
+    printf("\nMova o disco %d da base %c --> %c\n", qtd, origem, destino);
+    hanoi(qtd-1, aux, destino, origem); // Resolve para n-1 discos, movendo da aux para o destino
   }
 }
