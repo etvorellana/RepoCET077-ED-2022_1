7d67e0846802ced9b0bb55d9b2dabb79890de868
etvorellana <evalero@uesc.br>
2022-04-12 10:20:41 -0300

    Nova pasta de filas e pilhas da aula do dia 12/04

--- src/Listas_Lineares/Alocação_Sequencial/Exemplo_12/ferramentas.h
+++ src/Listas_Lineares/Alocação_Sequencial/Exemplo_12/ferramentas.h
@@ -10,7 +10,6 @@ typedef struct
     char email[100];  // eMail com até 99 caracteres
 } TAluno;
 
-
 // Manipulação de listas não ordenadas
 int buscaAluno_NOrd(TAluno lista[], int tam, int chave);
 int incAluno_NOrd(TAluno aluno, TAluno lista[], int *tam, int cap);
@@ -40,3 +39,27 @@ void ordenaLista(TListAlunos *listaA);
 
 void printLista(TAluno lista[], int tam);
 TListAlunos* geraListaDeAlunos(int tam, int cap, int eOrd);
+
+typedef struct{
+	TAluno* pilha;
+	int cap;  //pilha vazia quando topo = 0
+	int topo; //pilha cheia quando topo = cap
+}TPilhaAlunos; //last in first out - lifo
+
+// Pilhas
+void iniPilhaAlunos(TPilhaAlunos* pilha, int cap); 
+int empilharAluno(TAluno aluno, TPilhaAlunos* pilha); //push
+int desempilharALuno(TAluno* aluno, TPilhaAlunos* pilha); //pop 
+
+typedef struct{
+	TAluno* fila;
+	int cap;
+	int ini; 	//fila vazia quando ini = fim
+	int fim;  //fila cheia quando fim - ini = cap
+}TFilaAlunos; //first in first out - fifo
+
+// Filas
+void iniFilaAlunos(TFilaAlunos* fila, int cap);
+int insereNaFila(TAluno aluno, TFilaAlunos* fila);
+int removeDaFila(TAluno* aluno, TFilaAlunos* fila);
+
