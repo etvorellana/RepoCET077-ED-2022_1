21fe1f37396babbed9de29c01bf33d81f97b3fa9
CarlosHSMJr <chsmendonca.cic@uesc.br>
2022-04-19 08:56:44 -0300

    Lista 2

---
+++ equipe_2/Lista 2/ferramentas.h
@@ -0,0 +1,44 @@
+#define TRUE 1
+#define FALSE 0
+#define LSIZE 10
+
+// Armazenando as informações de um aluno
+typedef struct
+{
+    int numMatricula; //chave com 9 dígitos (INT_MAX +2147483647)
+    char nome[100];   // nome com até 99 caracteres
+    char email[100];  // eMail com até 99 caracteres
+} TAluno;
+
+
+// Manipulação de listas não ordenadas
+int buscaAluno_NOrd(TAluno lista[], int tam, int chave);
+int incAluno_NOrd(TAluno aluno, TAluno lista[], int *tam, int cap);
+int remAluno_NOrd(int chave, TAluno lista[], int *tam);
+
+// Manipulação de listas ordenadas
+int buscaAluno_Ord(TAluno lista[], int tam, int chave);
+int incAluno_Ord(TAluno aluno, TAluno lista[], int *tam, int cap);
+int remAluno_Ord(int chave, TAluno lista[], int *tam);
+
+typedef struct{
+	TAluno* lista;
+	int cap;
+	int tam;
+    int eOrd;
+} TListAlunos;
+
+// Utilizar isto aqui no projeto 
+void iniListAlunos(TListAlunos* lista, int cap, int eOrd);
+int buscaChaveNaLista(int chave, TListAlunos *lista);
+int incAlunoNaLista(TAluno aluno, TListAlunos *lista);
+int remAlunoDaLista(TAluno aluno, TListAlunos *lista,int *tam,int chave);
+void uniaoListas(TListAlunos *listaA, TListAlunos *listaB,TListAlunos *uniao);
+void difListas(TListAlunos *listaA, TListAlunos *listaB, TListAlunos *dif);
+void intListas(TListAlunos *listaA, TListAlunos *listaB,TListAlunos *inter);
+void ordenaLista(TListAlunos *listaA,int inicio,int fim);
+//void mergeSort(TListAlunos *listaA,TListAlunos *vetorTemp,int inicio,int fim)
+//void printLista(TAluno lista[], int tam);
+void geraListaDeAlunos(TListAlunos *lista, int tam, int cap, int eOrd);
+
+void printLista(TAluno lista[], int tam);
